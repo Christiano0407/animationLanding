00baf84da3515e2b0575b28c9db534815c5f2f5d
Christiano0407 <placebo0704@gmail.com>
2022-02-13 11:21:02 -0600

    slider

--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,23 @@ console.group("Animation Landing Page");
 // DOM Animation ===>
 const modal = document.querySelector(".modal");
 console.log(modal);
-const btnModal = document.querySelector("#btn");
+const btnModal = document.getElementById("btn");
 console.log(btnModal);
+const btnTwo = document.getElementById("btnTwo");
+const btnThree = document.getElementById("btnThree");
 const closeBtn = document.querySelector(".modal__container--close");
 console.log(closeBtn);
 /* console.log(close); */
 // Events ==>
 btnModal.addEventListener(`click`, () => {
+  modal.classList.remove("hidden");
+  modal.classList.add("visible");
+});
+btnTwo.addEventListener("click", () => {
+  modal.classList.remove("hidden");
+  modal.classList.add("visible");
+});
+btnThree.addEventListener("click", () => {
   modal.classList.remove("hidden");
   modal.classList.add("visible");
 });
